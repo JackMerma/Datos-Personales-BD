d2e0b44ef44ed6d35c11063cae57cabee94625c6
Jackson Merma <jmermap@unsa.edu.pe>
2022-07-20 00:35:27 -0500

    implementacion de vistaConsulta

--- src/vista/VistaConsulta.java
+++ src/vista/VistaConsulta.java
@@ -1,10 +1,16 @@
 package src.vista;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
+import java.sql.Connection;
+import java.sql.*;
+import src.modelo.Conexion;
 
 public class VistaConsulta {
 	public JPanel content;
@@ -20,6 +26,11 @@ public class VistaConsulta {
 	//entrada
 	public JTextField input = new JTextField();
 
+	//conexion
+	private Conexion con = new Conexion();
+	private PreparedStatement ps; 
+	private ResultSet rs;
+	private Connection bd;  
 
 	public JPanel getContent() {
 		content = new JPanel(new GridLayout(2,1,5,5));
@@ -46,7 +57,87 @@ public class VistaConsulta {
 		JPanel gridContentPanel = new JPanel(new GridLayout(1,2,5,5));
 
 		//listeners
+		ide.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String sql = "CALL Consulta_byIde(?);";
 
+				try{
+					con.conectar();
+					bd = con.getConnection();
+					ps = bd.prepareStatement(sql);
+					ps.setString(1, input.getText());//input
+					rs = ps.executeQuery();
+
+					String[] cols = {"Código", "Nombre", "Apellido", "Pais", "Numero", "Correo"};
+					Object[][] objetos = new Object[1][6];
+
+					//resultado
+					int i = 0;
+					while(rs.next()){
+						objetos[i][0] = rs.getInt(1);
+						objetos[i][1] = rs.getString(2);
+						objetos[i][2] = rs.getString(3);
+						objetos[i][3] = rs.getString(4);
+						objetos[i][4] = rs.getInt(5);
+						objetos[i][5] = rs.getString(6);
+						i++;
+					}
+
+					JTable tabla = new JTable(objetos, cols);
+					JScrollPane barra = new JScrollPane(tabla);
+
+					showNewFrame(barra);
+
+					System.out.println("Procedimiento almacenado Consulta_byIde:CORRECTO");
+				}catch(Exception ex){
+					System.out.println("Procedimiento almacenado Consulta_byIde:ERROR");
+					System.err.println(e);
+				}finally{
+					con.desconectar();
+				}
+			}
+		});
+
+		nombre.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String sql = "CALL Consulta_byNombre(?);";
+
+				try{
+					con.conectar();
+					bd = con.getConnection();
+					ps = bd.prepareStatement(sql);
+					ps.setString(1, input.getText());//input
+					rs = ps.executeQuery();
+
+					String[] cols = {"Código", "Nombre", "Apellido", "Pais", "Numero", "Correo"};
+					Object[][] objetos = new Object[20][6];//primeros 20
+
+					//resultado
+					int i = 0;
+					while(rs.next()){
+						objetos[i][0] = rs.getInt(1);
+						objetos[i][1] = rs.getString(2);
+						objetos[i][2] = rs.getString(3);
+						objetos[i][3] = rs.getString(4);
+						objetos[i][4] = rs.getInt(5);
+						objetos[i][5] = rs.getString(6);
+						i++;
+					}
+
+					JTable tabla = new JTable(objetos, cols);
+					JScrollPane barra = new JScrollPane(tabla);
+
+					showNewFrame(barra);
+
+					System.out.println("Procedimiento almacenado Consulta_byNombre:CORRECTO");
+				}catch(Exception ex){
+					System.out.println("Procedimiento almacenado Consulta_byNombre:ERROR");
+					System.err.println(e);
+				}finally{
+					con.desconectar();
+				}
+			}
+		});
 		//agregando botones
 		gridContentPanel.add(ide);
 		gridContentPanel.add(nombre);
@@ -64,5 +155,23 @@ public class VistaConsulta {
 			panel.add(new JLabel("   "), BorderLayout.EAST);
 		if (w)
 			panel.add(new JLabel("   "), BorderLayout.WEST);
+	}
+
+	public void centrarCeldas(JTable tabla) {
+		DefaultTableCellRenderer tcr = new DefaultTableCellRenderer();
+		tcr.setHorizontalAlignment(SwingConstants.CENTER);
+		for (int i = 0; i < tabla.getColumnCount(); i++) {
+			tabla.getColumnModel().getColumn(i).setCellRenderer(tcr);
+		}
+	}
+
+	public void showNewFrame(JScrollPane scroll){
+		JFrame newFrame = new JFrame("Consulta");
+		newFrame.setLocationRelativeTo(null);
+		newFrame.setLayout(new FlowLayout());
+		newFrame.setVisible(true);
+		newFrame.setSize(500,400);
+
+		newFrame.add(scroll);
 	}
 }
